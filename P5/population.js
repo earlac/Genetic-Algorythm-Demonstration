a064b3b4cb4a88fe2d758e7fd59919d7090ea46a
earlac <62187974+earlac@users.noreply.github.com>
2022-05-30 18:48:23 -0600

    1

---
+++ P5/population.js
@@ -0,0 +1,72 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/bGz7mv2vD6g
+
+function Population() {
+  // Array of individuos
+  this.individuos = [];
+  // Amount of individuos
+  this.cantIndividuos = 25;
+  // Amount padre rocket partners
+  this.listaPadres = [];
+
+  // Associates a rocket to an array index
+  for (var i = 0; i < this.cantIndividuos; i++) {
+    this.individuos[i] = new Rocket();
+  }
+
+  this.evaluate = function() {
+    var maxfit = 0;
+    // Iterate through all individuos and calcultes their fitness
+    for (var i = 0; i < this.cantIndividuos; i++) {
+      // Calculates fitness
+      this.individuos[i].calcFitness();
+      // If current fitness is greater than max, then make max equal to current
+      if (this.individuos[i].fitness > maxfit) {
+        maxfit = this.individuos[i].fitness;
+      }
+    }
+    // Normalises fitnesses
+    for (var i = 0; i < this.cantIndividuos; i++) {
+      this.individuos[i].fitness /= maxfit;
+    }
+
+    this.listaPadres = [];
+    // Take individuos fitness make in to scale of 1 to 100
+    // A rocket with high fitness will highly likely will be in the mating pool
+    for (var i = 0; i < this.cantIndividuos; i++) {
+      var n = this.individuos[i].fitness * 100;
+      for (var j = 0; j < n; j++) {
+        this.listaPadres.push(this.individuos[i]);
+      }
+    }
+
+
+  };
+  // Selects appropriate genes for child
+  this.selection = function() {
+    var nIndividuos = [];
+    for (var i = 0; i < this.individuos.length; i++) {
+      // Picks random dna
+      var padreA = random(this.listaPadres.slice(0,3)).dna;
+      var padreB = random(this.listaPadres).dna;
+      // Creates child by using cruzar function
+      var child = padreA.cruzar(padreB);
+      child.mutar();
+      // Creates new rocket with child dna
+      nIndividuos[i] = new Rocket(child);
+    }
+    // This instance of individuos are the new individuos
+    this.individuos = nIndividuos;
+  };
+
+  // Calls for actualizar and show functions
+  this.run = function() {
+    for (var i = 0; i < this.cantIndividuos; i++) {
+      this.individuos[i].actualizar();
+      // Displays individuos to screen
+      this.individuos[i].show();
+    }
+  };
+}
---
+++ P5/rocket.js
@@ -0,0 +1,88 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/bGz7mv2vD6g
+
+// Constructor function
+function Rocket(dna) {
+  // Physics of rocket at current instance
+  this.pos = createVector(width / 2, height);
+  this.vel = createVector();
+  this.acc = createVector();
+  // Checkes rocket has reached target
+  this.gano = false;
+  // Checks if rocket had choco
+  this.choco = false;
+  // Gives a rocket dna
+  if (dna) {
+    this.dna = dna;
+  } else {
+    this.dna = new DNA();
+  }
+  this.fitness = 0;
+
+  // Object can recieve force and add to acceleration
+  this.empujar = function(force) {
+    this.acc.add(force);
+  };
+  // Calulates fitness of rocket
+  this.calcFitness = function() {
+    // Takes distance to target
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+
+    // Maps range of fitness
+    this.fitness = map(d, 0, width, width, 0);
+    // If rocket gets to target increase fitness of rocket
+    if (this.gano) {
+      this.fitness *= 10;
+    }
+    // If rocket does not get to target decrease fitness
+    if (this.choco) {
+      this.fitness /= 10;
+    }
+  };
+  // actualizars state of rocket
+  this.actualizar = function() {
+    // Checks distance from rocket to target
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    // If distance less than 10 pixels, then it has reached target
+    if (d < 10) {
+      this.gano = true;
+      this.pos = target.copy();
+    }
+
+    // Rocket has hit left or right of window
+    if (this.pos.x > width || this.pos.x < 0) {
+      this.choco = true;
+    }
+    // Rocket has hit top or bottom of window
+    if (this.pos.y > height || this.pos.y < 0) {
+      this.choco = true;
+    }
+
+    //applies the random vectors defined in dna to consecutive frames of rocket
+    this.empujar(this.dna.genes[count]);
+    // if rocket has not got to goal and not choco then actualizar physics engine
+    if (!this.gano && !this.choco) {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+      this.vel.limit(4);
+    }
+  };
+  // displays rocket to window
+  this.show = function() {
+    // push and pop allow's rotating and translation not to affect other objects
+    push();
+    //color customization of individuos
+    noStroke();
+    fill(255, 150);
+    //translate to the postion of rocket
+    translate(this.pos.x, this.pos.y);
+    //rotatates to the angle the rocket is pointing
+    //creates a rectangle shape for rocket
+    rectMode(CENTER);
+    rect(0, 0, 25, 25);
+    pop();
+  };
+}
